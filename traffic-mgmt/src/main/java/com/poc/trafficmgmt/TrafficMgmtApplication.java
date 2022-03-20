1554270282dbf52c6b49aff4e36b0322f11420b0
bozzelliandrea <andrea_bozzelli@tria.it>
2022-03-20 12:04:22 +0100

    [POC] base project modules

---
+++ traffic-mgmt/src/main/java/com/poc/trafficmgmt/TrafficMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.poc.trafficmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficMgmtApplication.class, args);
+	}
+
+}
---
+++ traffic-mgmt/src/test/java/com/poc/trafficmgmt/TrafficMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.trafficmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
